87c5ec34e8d1611de077d475a1ff111e17b967dd
IngLeonardo <leonardosilva.ing@gmail.com>
2022-04-29 11:26:26 -0500

    Iniciando Express y conectando con Mongoose

---
+++ api.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var TaskModel = require('./task_schema');
+var router = express.Router();
+var query = "mongodb+srv://Task:leoasd@cluster0.njawv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const db = (query);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (error) {
+    if (error) {
+        console.log("Error!" + error);
+    } else {
+        console.log("Se ha conectado con la base de datos exitosamente");
+    }
+});
+
+module.exports = router;
